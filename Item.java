dccc2c48fe0ee2a0d45254cdff697cd76b18f293
reyanshsawant <136645029+reyanshsawant@users.noreply.github.com>
2025-03-27 13:12:27 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -3,9 +3,33 @@ public class Item {
   private String name;
   private String description;
 
-  public Item (double price, String name, String description) {
+  public Item(double price, String name, String description) {
     this.price = price;
     this.name = name;
+    this.description = description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
     this.description = description;
   }
 }
