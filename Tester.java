3bd720c4de1841fac087ff4bdf340598514614f0
reyanshsawant <136645029+reyanshsawant@users.noreply.github.com>
2025-03-27 12:52:05 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,3 +1,10 @@
 public class Item {
-  private 
+  private double price;
+  private String name;
+  private String description;
+
+  public Item (double price, String name, String description) {
+    this.price = price;
+    this.name = name;
+    this.description = description;
 }
